5dfe9198f6a644de5e2cbae3744ed8828fc06f1b
ezyxip <anzej2003@gmail.com>
2025-04-10 19:55:41 +0600

    chore: Настроена маршрутизация

--- app/routes.ts
+++ app/routes.ts
@@ -1,8 +1,24 @@
-import { type RouteConfig, index, layout, route } from "@react-router/dev/routes";
+import {
+    type RouteConfig,
+    index,
+    layout,
+    route,
+} from "@react-router/dev/routes";
 
 export default [
     layout("./theme/theme-wrapper.tsx", [
-        index("./routes/home.tsx"),
-        route("/dev", "dev/dev.tsx"),
-    ])
+        layout("./user/user-context.tsx", [
+            layout("./cell/cell-infra.tsx", [
+                layout("./cell/cell-registrator.tsx", [
+                    index("./routes/home.tsx"),
+                    route("/dev", "dev/dev.tsx"),
+                    layout("./auth-infra/auth-infra.tsx", [
+                        layout("./auth-infra/auth-registrator.tsx", [
+                            route("/auth", "routes/auth.tsx"),
+                        ]),
+                    ]),
+                ]),
+            ]),
+        ]),
+    ]),
 ] satisfies RouteConfig;
